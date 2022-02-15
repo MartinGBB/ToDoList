179bf809b780b0aa679041c56491d7c34e6a8847
Martin <escorpmartin97@gmail.com>
2022-02-15 00:18:12 -0300

    establece conexao com banco de dados "toDoList"

--- backend/src/model/connection.js
+++ backend/src/model/connection.js
@@ -1,9 +1,25 @@
 const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
-const { HOST } = process.env;
 
-const URL = `mongodb://${HOST || 'localhost'}:27017`;
-const CLIENT = new MongoClient(URL);
-const DB_NAME = 'toDoList';
-const connection = null;
+const HOST = process.env.HOST || 'localhost';
+const DB_NAME = process.env.DB_NAME || 'toDoList';
+
+const URL = `mongodb://${HOST}:27017/toDoList`;
+let connection = null;
+
+module.exports = async () => {
+  try {
+    connection = connection || (connection = (await MongoClient.connect(
+      URL,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    )).db(DB_NAME));
+    console.log('mongo connected');
+    return connection;
+  } catch (err) {
+    process.exit(1);
+  }
+};
