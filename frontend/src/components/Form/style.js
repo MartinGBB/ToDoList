733c3d80658ae605e16a7bfae5d50a435bdb7ac3
Martin <escorpmartin97@gmail.com>
2022-02-16 19:04:32 -0300

    style: estilo para botao

--- frontend/src/components/Form/style.js
+++ frontend/src/components/Form/style.js
@@ -20,5 +20,16 @@ export const Content = styled.form`
     border: 1px solid #bbb;
     border-radius: 3px;
     background: snow;
+    margin: 5px;
   };
-`
+  
+  & button {
+    width: auto;
+    padding: 5px;
+    border: 1px solid #bbb;
+    border-radius: 3px;
+    background: snow;
+    margin: 5px;
+    cursor: pointer;
+  };
+`;
