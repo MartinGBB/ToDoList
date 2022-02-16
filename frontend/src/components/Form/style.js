d9c05987e8ff04e9b25ffccbc492a61176a9bddd
Martin <escorpmartin97@gmail.com>
2022-02-16 16:48:13 -0300

    style: adiciona estilo ao contenedor do form usando style-components

--- frontend/src/components/Form/style.js
+++ frontend/src/components/Form/style.js
@@ -2,4 +2,14 @@ import styled from 'styled-components';
 
 export const Content = styled.form`
   background-color: blue;
+  display: flex;
+  align-items: center;
+  padding: 24px 28px;
+
+  & input {
+    width: 25%;
+    padding: 6px;
+    border: 1px solid #bbb;
+    border-radius: 3px;
+  }
 `
