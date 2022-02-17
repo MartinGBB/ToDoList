25b9cfb743c8420939e788f883bbaeefb89ab9c8
Martin <escorpmartin97@gmail.com>
2022-02-17 02:18:02 -0300

    fix: apaga compoente de mapear dados e mapeo dados e retorno eles apenas num componente

--- frontend/src/components/TaskList/index.js
+++ frontend/src/components/TaskList/index.js
@@ -1,10 +1,43 @@
-// import { } from './style';
-import Task from "../Task";
+import fetchApi from "../../utils/fetch";
+import { useState, useEffect } from 'react';
 
 function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleFind = async () => {
+    const route = "/";
+    const method = "GET";
+    const response = await fetchApi(route, method);
+    setTasks(response);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    handleFind();
+  }, [tasks]);
+
+  if (isLoading) return <h1>Loading</h1>;
   return (
     <div>
-      <Task />
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Categoria</th>
+            </tr>
+          </thead>
+            {
+              tasks.map(({ task, category }, i) => (
+              <tbody key={ i }>
+                <tr>
+                  <td>{ task }</td>
+                  <td>{ category }</td>
+                </tr>
+              </tbody>
+              ))
+            }
+          </table>
     </div>
   );
 }
