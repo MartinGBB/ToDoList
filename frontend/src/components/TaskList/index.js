e04ea72fcd67e713bd47143fd81c91ee65a175e0
Martin <escorpmartin97@gmail.com>
2022-02-17 00:35:45 -0300

    feat: cria componente para lista de tarefas

---
+++ frontend/src/components/TaskList/index.js
@@ -0,0 +1,11 @@
+// import { } from './style';
+
+function TaskList() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default TaskList;
--- frontend/src/pages/ToDoList/ToDoList.js
+++ frontend/src/pages/ToDoList/ToDoList.js
@@ -1,9 +1,11 @@
 import Form from '../../components/Form';
+import TaskList from '../../components/TaskList';
 
 function ToDoList() {
   return (
     <div>
       <Form />
+      <TaskList />
     </div>
   );
 }
