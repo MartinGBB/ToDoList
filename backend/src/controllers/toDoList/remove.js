245bf207cb128f42af8d5251e24a3ecbd831e08c
Martin <escorpmartin97@gmail.com>
2022-02-17 03:36:41 -0300

    feat: implementa controller

---
+++ backend/src/controllers/toDoList/remove.js
@@ -0,0 +1,12 @@
+const service = require('../../services/recipes');
+
+module.exports = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const remove = await service.remove(id);
+    const { status, message } = remove;
+    res.status(status).json(message);
+  } catch (error) {
+    next(error);
+  }
+};
