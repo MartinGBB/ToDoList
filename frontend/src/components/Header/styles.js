5724c066fb8a41b3d8ff5b2badc40c58ddb03dd0
Martin <escorpmartin97@gmail.com>
2022-05-23 18:05:20 -0300

    style: adiciona redondado ao header

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.form`
   background-color: var(--secundary-bg-color);
   border-bottom: 2px solid var(--main-bg-color);
-  height: 100%;
+  border-radius: 0px 20px 0px 20px;
+  height: 90%;
   color: white;
   font-family: 'Grape Nuts', cursive;
   `;
