2825d2d2620031e74e329fb2abf430ddf37196fe
Martin <escorpmartin97@gmail.com>
2022-05-31 21:14:49 -0300

    style: configura grid

--- frontend/src/pages/ToDoList/styles.js
+++ frontend/src/pages/ToDoList/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin: auto;
   width: 90%;
-  background-color: var(--third-bg-color);
+  /* background-color: var(--third-bg-color); */
   border-radius: 10px;
   margin-top: 1rem;
   margin-bottom: 1rem;
