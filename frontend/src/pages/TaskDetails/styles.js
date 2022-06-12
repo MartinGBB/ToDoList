feaaedeec615e930a3a5785338dcbe616d3e739b
Martin <escorpmartin97@gmail.com>
2022-06-11 21:39:30 -0300

    style: formata inputs

--- frontend/src/pages/TaskDetails/styles.js
+++ frontend/src/pages/TaskDetails/styles.js
@@ -11,4 +11,22 @@ export const Container = styled.div`
   position: fixed;
   top: 30vh;
   left: 20vw;
+
+  & input {
+    padding: 0 0.5em;
+    width: 19em;
+  }
+
+  select {
+    width: 20em;
+  }
+
+  & input, select {
+    height: 2em;
+    border-radius: 5px;
+    display: block;
+    margin: 0.7em auto;
+  }
+  
+  &
 `;
