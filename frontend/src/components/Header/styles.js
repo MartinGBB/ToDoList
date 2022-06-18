41573405882704fee545fe24309b54efd94d4afd
Martin <escorpmartin97@gmail.com>
2022-06-18 14:16:27 -0300

    style: elimina fundo do header

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -5,16 +5,12 @@ export const Container = styled.form`
   grid-column-end: 5;
   grid-row-start: 1;
   grid-row-end: 2;
-  background-color: var(--secundary-bg-color);
-  border-bottom: 2px solid var(--main-bg-color);
-  border-radius: 0px 0 20px 20px;
-  height: 4rem;
-  justify-content: center;
   display: flex;
   color: white;
   font-family: 'Grape Nuts', cursive;
 
   & h1 {
     margin: auto;
+    font-size: 2rem;
   }
   `;
